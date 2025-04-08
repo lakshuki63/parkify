424a93a0d4318401034d0e723e3ae3d13651b24c
Nakul <agentvasco69@gmail.com>
2025-04-09 00:53:13 +0530

    changes to forget password

--- forgetpassword page/fg.php
+++ forgetpassword page/fg.php
@@ -1,45 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Reset Password</title>
     <link rel="stylesheet" href="forget.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body >
+<body>
     <div class="top">
-    <header class="hero">
-        <h1 class="hero-title"><u> RESET Password</u></h1>
-        <p class="hero-subtitle"></p>
-      </header>
+        <header class="hero">
+            <h1 class="hero-title">RESET PASSWORD</h1>
+            <p class="hero-subtitle">Enter your email and solve the captcha to proceed</p>
+        </header>
     </div>
-    <form >
+
+    <form>
         <div class="box">
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email">
-         <br> <br><br>
-        <label for="captcha">CAPTCHA CODE:</label>
-        <div class="captcha-box" id="captcha"></div>
-        <!-- <button onclick="generateCaptcha()"><i class="fa-solid fa-rotate"></i></button> -->
-        <div class="icon"><i class="fa-solid fa-rotate" onclick="generateCaptcha()"></i></div>
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email" placeholder="Enter your email"><br><br>
 
-        
-        <br>
-        <br><br>
-        <label for="">enter:</label>
-        
-        <input type="text" name="captchaInput" id="captchaInput">
-        <br><br><br>
+            <label for="captcha">CAPTCHA CODE:</label>
+            <div class="captcha-box" id="captcha">A7X9P</div>
 
-        <button id="submit" >Submit</button>
-        <p id="message"></p>
+            <div class="icon">
+                <i class="fa-solid fa-rotate" onclick="generateCaptcha()"></i>
+            </div>
+            <br>
 
+            <label for="captchaInput">Enter CAPTCHA:</label>
+            <input type="text" name="captchaInput" id="captchaInput" placeholder="Enter CAPTCHA code"><br><br>
 
+            <button id="submit">Submit</button>
+            <p id="message"></p>
         </div>
+    </form>
 
     <script src="fg.js"></script>
-        </form>
-    
 </body>
 </html>
