dfa93bc19b5e536632bf49bc02cccb6435019170
Nakul <agentvasco69@gmail.com>
2025-04-08 20:49:25 +0530

    made changes to register.php

--- registeration/register.php
+++ registeration/register.php
@@ -1,42 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="reg.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Parkify Login</title>
+  <link rel="stylesheet" href="reg.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
+  <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&family=Michroma&display=swap" rel="stylesheet">
 </head>
-<body >
-    <div class="top">
+<body>
+  <div class="top">
     <header class="hero">
-        <h1 class="hero-title"><u> Parkify:</u></h1>
-        <p class="hero-subtitle">Smart ,Effortless & Secure Parking, Anytime</p>
-      </header>
+      <div class="brand">
+        <i class="fa-solid fa-car-side car-icon"></i>
+        <h1 class="hero-title">Parkify</h1>
+      </div>
+      <p class="hero-subtitle">Smart, Effortless & Secure Parking, Anytime</p>
+    </header>
+    
+  </div>
+  <div class="car-radar-container">
+    <span class="radar-ping"></span>
+    
+  </div>
+  
+  
+
+  <form action="ifok.php" method="POST">
+    <div class="box">
+      <label for="userName">USER-NAME:</label>
+      <input type="text" name="userName" id="userName" />
+
+      <br /><br /><br />
+
+      <label for="password">PASSWORD:</label>
+      <input type="password" name="password" id="password" />
+
+      <p><a href="..\forgetpassword page\fg.php">Forget Password?</a></p>
+
+      <button id="submit">SUBMIT</button>
     </div>
-    <form action="ifok.php" method="POST">
-        <div class="box">
-        <label for="userName">USER-NAME:</label>
-        <input type="text" name="userName" id="userName">
-        <br><br><br>
 
+    <p class="signup">
+      <span class="new">New User?</span>
+      <a href="..\userName\userName.php" class="signup-link">Sign Up Here</a>
+      <br />
+      <span class="or">Or login using</span>
+    </p>
 
-        <label for="email">PASSWORD:</label>
-        <input type="password" name="password" id="password">
-        
-            <p><a href="..\forgetpassword page\fg.php"> Forget Password ?</a></p>
-        <button id="submit"><b>SUBMIT</b> </button>
-        </div>
-
-                  
-        <p class="signup">New User?<a href="..\userName\userName.php">Sign Up Here</a> Or login using</p>
-        
-        <div class="icon">
-        <i class="fa-brands fa-google" id="google"></i>
-        <i class="fa-brands fa-facebook" id="facebook"></i>
-        <i class="fa-brands fa-apple" id="apple"></i>
-        </div>
-    </form>
-
+    <div class="icon">
+      <i class="fa-brands fa-google" id="google"></i>
+      <i class="fa-brands fa-facebook" id="facebook"></i>
+      <i class="fa-brands fa-apple" id="apple"></i>
+    </div>
+  </form>
 </body>
 </html>
