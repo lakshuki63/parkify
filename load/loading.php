53f7ea2b8c68a40ca1ff95721dbe905b53d91fc1
sidii1 <sidii0080@gmail.com>
2025-04-09 15:47:34 +0530

    register

--- load/loading.php
+++ load/loading.php
@@ -4,7 +4,7 @@
         <title>Loading</title>
         <link rel="stylesheet" href="load.css"> 
         <!-- one must copy relative path of css file -->
-        <meta http-equiv="refresh" content="3;../registeration/register.php"> 
+        <meta http-equiv="refresh" content="3;../registration/register.php"> 
     </head>
     <body>
         <div class="container-load">
