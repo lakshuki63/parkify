0960ec71e5d7da88f3853e505a95cf354c45a9c9
sidii1 <sidii0080@gmail.com>
2025-04-16 11:12:23 +0530

    commit

--- book/booking.php
+++ book/booking.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <title>Car Parking Slot Booking</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <link rel="stylesheet" href="book.css">
 </head>
 <body>
